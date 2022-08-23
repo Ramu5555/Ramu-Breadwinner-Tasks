10ba2ebc53971784fc59a3a510b87fe0fdac2835
Ramu5555 <91936935+Ramu5555@users.noreply.github.com>
2022-08-23 19:13:29 +0530

    Update invoiceRelatedList.js

--- invoiceRelatedList.js
+++ invoiceRelatedList.js
@@ -1,6 +1,9 @@
 import { LightningElement, wire, track, api } from 'lwc';
-import getinvoiceRelaredList from '@salesforce/apex/InvoiceRelatedListController.getinvoiceRelaredList';
-import Status from '@salesforce/schema/Case.Status';
+import getinvoiceRelatedList from '@salesforce/apex/InvoiceRelatedListController.getinvoiceRelatedList';
+import totalPaidInvoices from '@salesforce/apex/InvoiceRelatedListController.totalPaidInvoices';
+import totalDueInvoices from '@salesforce/apex/InvoiceRelatedListController.totalDueInvoices';
+import totalReceivablesAmount from '@salesforce/apex/InvoiceRelatedListController.totalReceivablesAmount';
+import totalOverdueInvoices from '@salesforce/apex/InvoiceRelatedListController.totalOverdueInvoices';
 const COLUMNS = [
     { label: 'Invoice Number', fieldName: 'invName', type: 'url', typeAttributes: {label: { fieldName: 'Name' }, target: '_blank' }},
     { label: 'Invoice Date', fieldName: 'Invoice_Date__c', type: 'Date' },
@@ -8,16 +11,23 @@ const COLUMNS = [
     { label: 'Amount Due', fieldName: 'Amount_Due__c', type: 'currency' },
     { label: 'Amount Paid', fieldName: 'Amount_Paid__c', type: 'currency'},
     { label: 'Status', fieldName: 'Status__c', type: 'text'},
-    { label: 'Total', fieldName: 'Total__c', type: 'currency'}
+    { label: 'Total', fieldName: 'Total__c', type: 'currency'},
+    { label: 'Days Overdue', fieldName: 'Days_Overdue__c', type: 'number'}
 ];
 export default class InvoiceRelatedList extends LightningElement 
 {
     listofInvoices = [];
+    overduedata = [];
+    overdue1_30 = 0;
+    overdue31_60 = 0;
+    overdue61_90 = 0;
+    overdue90 = 0;
+    totalOverdue = 0;
     @api recordId;
     columns = COLUMNS;
-    @wire(getinvoiceRelaredList,{accountId:'$recordId'})
-    invoices({ error, data }) {
-
+    @wire(getinvoiceRelatedList,{accountId:'$recordId'})
+    invoices({ error, data }) 
+    {
         if (data) {
             console.log('Number of invoices'+data.length);
             let tempInvoiceList = []; 
@@ -33,6 +43,38 @@ export default class InvoiceRelatedList extends LightningElement
 
         } else if (error) {
             this.error = result.error;
+        }
+    }
+    @wire(totalPaidInvoices,{accountId:'$recordId'})paidInvoices;
+    @wire(totalDueInvoices,{accountId:'$recordId'})dueInvoices;
+    @wire(totalReceivablesAmount,{accountId:'$recordId'})totalReceivables;
+    @wire(totalOverdueInvoices,{accountId:'$recordId'})
+    overdue({ error, data })
+    {
+        if(data)
+        {
+            console.log("entered");
+            this.overduedata = data;
+            this.totalOverdue = this.overduedata.length;
+            for(let i=0; i<this.overduedata.length; i++)   
+       {
+         if(this.overduedata[i].Days_Overdue__c >=1 && this.overduedata[i].Days_Overdue__c <= 30)
+         {
+                this.overdue1_30 = this.overdue1_30 + 1;
+         }
+         else if(this.overduedata[i].Days_Overdue__c >=31 && this.overduedata[i].Days_Overdue__c <= 60)
+         {
+            this.overdue31_60 = this.overdue31_60 + 1;
+         }
+         else if(this.overduedata[i].Days_Overdue__c >=61 && this.overduedata[i].Days_Overdue__c <= 90)
+         {
+            this.overdue61_90 = this.overdue61_90 + 1;
+         }
+         else if(this.overduedata[i].Days_Overdue__c >=91)
+         {
+            this.overdue90 = this.overdue90 + 1;
+         }
+       }
         }
     }
 }
